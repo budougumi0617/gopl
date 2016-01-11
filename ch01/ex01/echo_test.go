ec594e26c5e24d24f3849b6f948eab7b0f8377c4
budougumi0617 <budougumi0617@gmail.com>
2016-01-11 19:15:28 +0900

    Modify incorrect test case

--- ch01/ex01/echo_test.go
+++ ch01/ex01/echo_test.go
@@ -12,9 +12,9 @@ func TestEcho(t *testing.T) {
 		expected string
 	}{
 		{[]string{"echo"}, "echo\n"},
-		{[]string{"echo one", "two", "three"}, "echo one two three\n"},
-		{[]string{"echo a", "b", "c"}, "echo a b c\n"},
-		{[]string{"echo 1", "2", "3"}, "echo 1 2 3\n"},
+		{[]string{"echo", "one", "two", "three"}, "echo one two three\n"},
+		{[]string{"echo", "a", "b", "c"}, "echo a b c\n"},
+		{[]string{"echo", "1", "2", "3"}, "echo 1 2 3\n"},
 	}
 
 	for _, test := range tests {
