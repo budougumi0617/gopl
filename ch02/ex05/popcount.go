99b675ae70da1e2a2a846947c6965bdf4820971a
budougumi0617 <budougumi0617@gmail.com>
2016-02-11 15:47:02 +0900

    Added ch02 ex03, ex04, ex05

---
+++ ch02/ex05/popcount.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+// pc[i] is the population count of i.
+var pc [256]byte
+var stdout io.Writer = os.Stdout // modified during testing
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount returns the population count (number of set bits) of x.
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+// PopCountByClear using clear rightmost.
+func PopCountByClear(x uint64) int {
+	n := 0
+	for x != 0 {
+		x = x & (x - 1) // clear rightmost non-zero bit
+		n++
+	}
+	return n
+}
+
+func main() {
+	n, _ := strconv.ParseUint(os.Args[1], 0, 64)
+	fmt.Fprintf(stdout, "result %v\n", PopCountByClear(n))
+}
