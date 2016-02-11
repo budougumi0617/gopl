f699a09103a3f4979576b296fd83c4d59558b3e8
budougumi0617 <budougumi0617@gmail.com>
2016-02-11 18:45:58 +0900

    Added ch01ex08, ch01ex09

---
+++ ch01/ex08/fetch_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	exit := m.Run()
+	if exit != 0 {
+		//os.Exit(exit)
+		fmt.Printf("result:%v", exit)
+	}
+}
+
+func TestFetch(t *testing.T) {
+	var tests = []struct {
+		args     []string
+		expected string
+	}{
+		{[]string{"fetch", "http://github.com/budougumi0617"}, "resp.status 200 OK\n"},
+		{[]string{"fetch", "https://github.com/budougumi0617"}, "resp.status 200 OK\n"},
+		{[]string{"fetch", "github.com/budougumi0617"}, "resp.status 200 OK\n"},
+		//{[]string{"fetch", "https://"}, "\n"},
+	}
+
+	for _, test := range tests {
+		os.Args = test.args
+		stdout = new(bytes.Buffer) // captured output
+		stderr = stdout
+		main()
+		got := stdout.(*bytes.Buffer).String()
+		if !strings.Contains(got, test.expected) {
+			t.Errorf("Result = %q, Expected %q", got, test.expected)
+		}
+	}
+}
--- ch01/ex09/README.md
+++ ch01/ex09/README.md
@@ -4,3 +4,16 @@ Modify `fetch` to also print the HTTP status code, found in `resp.Status`.
 ---
 # 練習問題1.9
 `fetch`を修正して、`resp.Status`に設定されているHTTPステータスコードも表示するようにしなさい。
+
+
+# Result
+
+```sh
+$  go run fetch.go http://google.com
+[http://google.com]resp.status 200 OK
+<!doctype html><html itemscope="" itemtype="http://schema.org/WebPage" lang="ja"><head><meta content="���E������������邽�߂̃c�[����񋟂��Ă��܂��B���܂��܂Ȍ���@�\��p���āA���T��������Ă��������B" name=...
+
+$  go run fetch.go http://google.com/foo
+[http://google.com/foo]resp.status 404 Not Found
+<html><body><h1>404 Not Found</h1></body></html>
+```
