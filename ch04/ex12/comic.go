55f5022af8529bc512f3e95ef18e93e6383e2e8b
budougumi0617 <budougumi0617@gmail.com>
2016-03-09 20:05:48 +0900

    Add ch04ex12

---
+++ ch04/ex12/comic.go
@@ -0,0 +1,27 @@
+package main
+
+// URL http://xkcd.com
+const URL = "http://xkcd.com/"
+
+// JSONFILE name of JSON in http://xkcd.com
+const JSONFILE = "/info.0.json"
+
+// ComicSearchResult result
+type ComicSearchResult struct {
+	TotalCount int
+	Items      []*Comic
+}
+
+// Comic JSON structure for http://xkcd.com
+type Comic struct {
+	Month      string
+	Num        int
+	Link       string
+	Year       string
+	News       string
+	Transcript string
+	SafeTitle  string `json:"safe_title"`
+	Alt        string
+	Image      string
+	Day        string
+}
---
+++ ch04/ex12/xkcd_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	DEBUG = true
+	var tests = []struct {
+		input    *bytes.Buffer
+		expected string
+	}{
+		{bytes.NewBufferString("\\q"), "Exit xkcd"},
+		{bytes.NewBufferString("stick\n\\q"), "http://xkcd.com/9/"},
+	}
+	for _, test := range tests {
+		stdout = new(bytes.Buffer) // captured output
+		stdin = test.input
+		main()
+		got := stdout.(*bytes.Buffer).String()
+		if !strings.Contains(got, test.expected) {
+			t.Errorf("Actual :%q, Expected %q", got, test.expected)
+		}
+	}
+}
+
+func TestGetLastPage(t *testing.T) {
+	if result, err := getLastPage(); err != nil || result < 0 {
+		t.Errorf("num = %v, err = %v", result, err)
+	}
+}
