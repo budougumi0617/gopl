349af12ad676ed36d4e276f3acc30a6110a24313
budougumi0617 <budougumi0617@gmail.com>
2016-03-09 21:37:13 +0900

    Add ch03ex05

--- ch03/ex05/README.md
+++ ch03/ex05/README.md
@@ -3,3 +3,12 @@
 ---
 # 練習問題 3.5
 `image.NewRGBA`関数と`color.RGBA`型あるいは`color.YbCr`型を使用してフルカラーのマンテルブロ集合を実装しなさい。
+
+
+# Result
+
+````sh
+$  go run mandelbrot.go > mandelbrot.png
+````
+
+![mandelbrot with color](./mandelbrot.png)
---
+++ ch03/ex05/mandelbrot.go
@@ -0,0 +1,49 @@
+// Mandelbrot emits a PNG image of the Mandelbrot fractal.
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"io"
+	"math/cmplx"
+	"os"
+)
+
+var stdout io.Writer = os.Stdout // modified during testing
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			// Image point (px, py) represents complex value z.
+			img.Set(px, py, mandelbrot(z))
+		}
+	}
+	png.Encode(stdout, img) // NOTE: ignoring errors
+}
+
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			r := 255 - contrast*n
+			g := contrast * n
+			b := g
+			return color.RGBA{r, g, b, 255}
+		}
+	}
+	return color.Black
+}
