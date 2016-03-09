2f0b9cd12ec0f24b87a513602086e1873cc8e9bf
budougumi0617 <budougumi0617@gmail.com>
2016-03-09 21:58:09 +0900

    Add ch03ex02, ch03ex07

---
+++ ch03/ex02/surface.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"math"
+	"os"
+)
+
+var stdout io.Writer = os.Stdout // modified during testing
+var stderr io.Writer = os.Stderr // modified during testing
+
+const (
+	width, height = 600, 320            // canvas size in pixels
+	cells         = 100                 // number of grid cells
+	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // pixels per x or y unit
+	zscale        = height * 0.4        // pixels per z unit
+	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
+func main() {
+	fmt.Fprintf(stdout, "<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay, aok := corner(i+1, j)
+			bx, by, bok := corner(i, j)
+			cx, cy, cok := corner(i, j+1)
+			dx, dy, dok := corner(i+1, j+1)
+			if aok && bok && cok && dok {
+				fmt.Fprintf(stdout, "<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+					ax, ay, bx, by, cx, cy, dx, dy)
+			}
+		}
+	}
+	fmt.Fprintln(stdout, "</svg>")
+}
+
+func corner(i, j int) (float64, float64, bool) {
+	// Find point (x,y) at corner of cell (i,j).
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// Compute surface height z.
+	z := fMogul(x, y)
+
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+
+	return sx, sy, true
+}
+
+func fMogul(x, y float64) float64 {
+	//math.Sin(-x) * math.Pow(1.5, -r)
+	//math.pow(2, math.Sin(y))*math.Pow(2, math.Sin(x))/12
+	//math.Sin(x*y/10)/10
+	return math.Pow(2, math.Sin(y)) * math.Pow(2, math.Sin(x)) / 20
+}
---
+++ ch03/ex02/surface_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	stdout = new(bytes.Buffer) // captured output
+	main()
+	got := stdout.(*bytes.Buffer).String()
+	if strings.Contains(got, "NaN") {
+		t.Errorf("SVG included NaN value.\n%s", got)
+	}
+}
