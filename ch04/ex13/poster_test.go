474751ebcbdb0defc03596bd37e61d253744b370
budougumi0617 <budougumi0617@gmail.com>
2016-03-09 18:06:49 +0900

    Add ch04ex13

---
+++ ch04/ex13/poster_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	var tests = []struct {
+		args     []string
+		filename string
+	}{
+		{[]string{"poster", "tron"}, "./TRON.jpg"},
+	}
+	for _, test := range tests {
+		os.Args = test.args
+		main()
+		if _, err := os.Stat(test.filename); err != nil {
+			t.Errorf("%v is not exist, %v", test.filename, err)
+		}
+	}
+}
+
+func TestSearchPoster(t *testing.T) {
+	var tests = []struct {
+		keyword  []string
+		expected Poster
+	}{
+		{[]string{"tron"}, Poster{"TRON", "http://ia.media-imdb.com/images/M/MV5BMTY0OTM4ODM2MF5BMl5BanBnXkFtZTgwMTI0NDIxMDE@._V1_SX300.jpg"}},
+	}
+	for _, test := range tests {
+		result, err := SearchPoster(test.keyword)
+		if err != nil {
+			t.Errorf("%v", err)
+		} else if result.Poster != test.expected.Poster {
+			t.Errorf("Result:%q, Expected:%q", result.Poster, test.expected.Poster)
+		} else if result.Title != test.expected.Title {
+			t.Errorf("Result:%q, Expected:%q", result.Title, test.expected.Title)
+		}
+	}
+}
+
+func TestSavePoster(t *testing.T) {
+	var tests = []struct {
+		title string
+		url   string
+	}{
+		{"result", "https://github.com/budougumi0617/zsh-presentation/blob/master/image/display.jpg"},
+	}
+	for _, test := range tests {
+		p := Poster{test.title, test.url}
+		SavePoster(p)
+		if _, err := os.Stat("./" + test.title + ".jpg"); err != nil {
+			t.Errorf("%v is not exist, %v", test.title, err)
+		}
+	}
+}
