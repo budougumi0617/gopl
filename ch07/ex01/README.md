99fa93cd1bd9810dd92ae530eb011e75f6a89556
budougumi0617 <budougumi0617@gmail.com>
2016-04-18 20:39:33 +0900

    Add ch07ex01

---
+++ ch07/ex01/README.md
@@ -0,0 +1,6 @@
+# Exercise 7.1
+Using the ideas from `ByteCounter`, implement counters for words and for lines. You will find `bufio.ScanWords` useful.
+
+---
+# 練習問題 7.1
+`ByteCounter`の考え方を利用して、ワードと行に対するカウンタを実装しなさい。`bufio.ScanWords`が役立つでしょう。
---
+++ ch07/ex01/main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import "testing"
+
+func TestWordsCounter(t *testing.T) {
+	var tests = []struct {
+		words    []byte
+		expected int
+	}{
+		{[]byte(""), 0},
+		{[]byte("one"), 1},
+		{[]byte("one two\nthree four"), 4},
+		{[]byte("one two \nthree four"), 4},
+	}
+	for _, test := range tests {
+		var wc WordsCounter
+		wc.Write(test.words)
+		if int(wc) != test.expected {
+			t.Errorf("Result = %v, Expected %v", wc, test.expected)
+		}
+	}
+}
+
+func TestLinesCounter(t *testing.T) {
+	var tests = []struct {
+		lines    []byte
+		expected int
+	}{
+		{[]byte(""), 0},
+		{[]byte("one"), 1},
+		{[]byte("one\n  two"), 2},
+		{[]byte("one \ntwo\n\nfour"), 4},
+		{[]byte("one \ntwo\n\nfour\n"), 4},
+	}
+	for _, test := range tests {
+		var lc LinesCounter
+		lc.Write(test.lines)
+		if int(lc) != test.expected {
+			t.Errorf("Result = %v, Expected %v", lc, test.expected)
+		}
+	}
+}
+
+func Example_main() {
+	main()
+
+	// Output:
+	// Count 3
+	// Count 4
+}
