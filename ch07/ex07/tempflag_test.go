b6f17057e773a769c501f001d4196460482d547b
budougumi0617 <budougumi0617@gmail.com>
2016-06-05 21:47:54 +0900

    Split code

--- ch07/ex07/tempflag_test.go
+++ ch07/ex07/tempflag_test.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"bytes"
-	"flag"
-	"fmt"
-	"io"
 	"os"
 	"testing"
 )
@@ -15,8 +12,9 @@ func TestLimitReader(t *testing.T) {
 		expected string
 	}{
 		{[]string{"dumy"}, "20°C added by Celsius.String()\n"},
+		{[]string{"dumy", "-temp", "20°C"}, "20°C added by Celsius.String()\n"},
+		{[]string{"dumy", "-temp", "32F"}, "0°C added by Celsius.String()\n"},
 	}
-	main()
 	for _, test := range tests {
 		os.Args = test.args
 		stdout = new(bytes.Buffer) // captured output
@@ -28,60 +26,3 @@ func TestLimitReader(t *testing.T) {
 
 	}
 }
-
-var stdout io.Writer = os.Stdout // modified during testing
-var stderr io.Writer = os.Stderr // modified during testing
-
-var temp = CelsiusFlag("temp", 20.0, "the temperature")
-
-func main() {
-	flag.Parse()
-	fmt.Fprintln(stdout, *temp)
-}
-
-type celsiusFlag struct {
-	Celsius
-}
-
-func (f *celsiusFlag) Set(s string) error {
-	var unit string
-	var value float64
-	fmt.Sscanf(s, "%f%s", &value, &unit) // no error check needed
-	switch unit {
-	case "C", "°C":
-		f.Celsius = Celsius(value)
-		return nil
-	case "F", "°F":
-		f.Celsius = FToC(Fahrenheit(value))
-		return nil
-	}
-
-	return fmt.Errorf("invalid temprature %q", s)
-}
-
-// CelsiusFlag defines a Celsius flag with the specified name,
-// default value, and usage, and returns the address of the flag variable.
-// The flag argument must have a quantity and a unit, e.g., "100C".
-func CelsiusFlag(name string, value Celsius, usage string) *Celsius {
-	f := celsiusFlag{value}
-	flag.CommandLine.Var(&f, name, usage)
-	return &f.Celsius
-}
-
-// Celsius For C
-type Celsius float64
-
-// Fahrenheit For F
-type Fahrenheit float64
-
-// Temperature type
-const (
-	AbsoluteZeroC Celsius = -273.15
-	FreezingC     Celsius = 0
-	BoilingC      Celsius = 100
-)
-
-func (c Celsius) String() string { return fmt.Sprintf("%g°C added by Celsius.String()", c) }
-
-// FToC converts a Fahrenheit temperature to Celsius.
-func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
