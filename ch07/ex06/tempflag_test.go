217af7b71570a163cb851631f9ea74664986210e
budougumi0617 <budougumi0617@gmail.com>
2016-06-05 21:15:24 +0900

    Add ch07ex06

---
+++ ch07/ex06/tempflag_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+	"os"
+	"testing"
+)
+
+func TestLimitReader(t *testing.T) {
+	var tests = []struct {
+		args     []string
+		expected string
+	}{
+		{[]string{"dumy", "-temp", "0K"}, "-273.15°C\n"},
+		{[]string{"dumy", "-temp", "0°C"}, "0°C\n"},
+		{[]string{"dumy", "-temp", "32F"}, "0°C\n"},
+	}
+	for _, test := range tests {
+		os.Args = test.args
+		stdout = new(bytes.Buffer) // captured output
+		main()
+		got := stdout.(*bytes.Buffer).String()
+		if got != test.expected {
+			t.Errorf("Result = %s, Expected %s", got, test.expected)
+		}
+
+	}
+}
