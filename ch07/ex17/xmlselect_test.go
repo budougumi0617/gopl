0ebafca95fcdb98cb3c2fd5036e07079af82982b
budougumi0617 <budougumi0617@gmail.com>
2016-06-10 03:45:22 +0900

    Add ch07ex17

---
+++ ch07/ex17/xmlselect_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+)
+
+func ExampleMain() {
+	source, _ := ioutil.ReadFile("./ex17.xml")
+	stdin = bytes.NewBuffer(source)
+	os.Args = []string{"test", "div", "foo", "url"}
+	main()
+	source, _ = ioutil.ReadFile("./ex17.xml")
+	stdin = bytes.NewBuffer(source)
+	os.Args = []string{"test", "div", "div2", "url"}
+	main()
+
+	// Output:
+	// div foo url: OK!!
+	// div div2 url: OK!!
+}
