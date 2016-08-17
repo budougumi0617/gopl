a9f2425447b4afda80b327f064711debda6248d7
budougumi0617 <budougumi0617@gmail.com>
2016-08-17 14:42:26 +0900

    Add ch08ex04

---
+++ ch08/ex04/reverb2.go
@@ -0,0 +1,52 @@
+// Copyright 2016 budougumi0617 All Rights Reserved.
+
+// Reverb2 is a TCP server that simulates an echo.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	wg := sync.WaitGroup{}
+	for input.Scan() {
+		wg.Add(1)
+		go func() {
+			echo(c, input.Text(), 1*time.Second)
+			wg.Done()
+		}()
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	wg.Wait()
+	c.Close()
+}
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn)
+	}
+}
