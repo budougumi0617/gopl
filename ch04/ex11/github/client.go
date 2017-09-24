6c366af9de1c44b1d86f23279d4536e45ed69dc6
budougumi0617 <budougumi0617@gmail.com>
2017-09-24 20:44:10 +0900

    W.I.P. Add Query.

--- ch04/ex11/github/client.go
+++ ch04/ex11/github/client.go
@@ -1,12 +1,21 @@
 // Copyright 2017 budougumi0617 All Rights Reserved.
 
+// Package github is GitHub API.
 package github
 
 import (
+	"bufio"
+	"fmt"
 	"net/http"
 	"net/url"
+	"os"
+	"strings"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
 )
 
+// Client is base struct for GitHub API.
 type Client struct {
 	URL        *url.URL
 	HTTPClient *http.Client
@@ -14,6 +23,18 @@ type Client struct {
 	Username, Password string
 }
 
-func NewClient(url, username, password string) (*Client, err) {
-	return nil, _
+// Query sets username and passward.
+func (c *Cient) Query() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Username: ")
+	username, _ := reader.ReadString('\n')
+	c.Username = strings.TrimSpace(username)
+
+	fmt.Print("Password: ")
+	bytesPassword, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		fmt.Printf("\nPassword error %v\n", err)
+	}
+	c.Password = bytePassword
 }
